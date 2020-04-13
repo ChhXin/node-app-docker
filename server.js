621b8bd8936a644bf934b73a6826b8e5ee9ad999
ChhXin <79379732@qq.com>
2020-04-13 21:42:25 +0800

    fix

--- server.js
+++ server.js
@@ -3,12 +3,12 @@
 const express = require("express");
 
 // Constants
-const PORT = 8888;
+const PORT = 8080;
 const HOST = "0.0.0.0";
 
 // App
 const app = express();
-app.get("/", (req, res) => {
+app.get("/dashboard", (req, res) => {
   res.send("Hello World");
 });
 
